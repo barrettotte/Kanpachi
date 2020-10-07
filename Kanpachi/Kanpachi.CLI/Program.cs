b34985697f46ad9b1a86d437d919f2f9a094e6af
Barrett <barrettotte@gmail.com>
2020-10-06 20:28:19 -0400

    update actions

--- Kanpachi/Kanpachi.CLI/Program.cs
+++ Kanpachi/Kanpachi.CLI/Program.cs
@@ -13,10 +13,10 @@ namespace Kanpachi.CLI{
             var parser = new Parser(with => with.HelpWriter = null);
             var parserResult = parser.ParseArguments<ConfigGet, ConfigSet, ConfigLs, ConfigReset>(baseCmd.SubArgs);
             parserResult
-                .WithParsed<ConfigGet>((x)   => Console.WriteLine($"Get config['{x.Key}']"))
-                .WithParsed<ConfigLs>((x)    => Console.WriteLine($"list config"))
-                .WithParsed<ConfigReset>((x) => Console.WriteLine($"reset config['{x.Key}']"))
-                .WithParsed<ConfigSet>((x)   => Console.WriteLine($"Set config['{x.Key}'] = '{x.Value}'"))
+                .WithParsed<ConfigGet>(x => Console.WriteLine($"Get config['{x.Key}']"))
+                .WithParsed<ConfigLs>(x => Console.WriteLine($"list config"))
+                .WithParsed<ConfigReset>(x => Console.WriteLine($"reset config['{x.Key}']"))
+                .WithParsed<ConfigSet>(x => Console.WriteLine($"Set config['{x.Key}'] = '{x.Value}'"))
                 .WithNotParsed(x => {
                     var helpText = HelpText.AutoBuild(parserResult, ht => {
                         ht.AutoHelp = false;
@@ -31,9 +31,9 @@ namespace Kanpachi.CLI{
             var parser = new Parser(with => with.HelpWriter = null);
             var parserResult = parser.ParseArguments<ExecCl, ExecShell, ExecSql>(baseCmd.SubArgs);
             parserResult
-                .WithParsed<ExecCl>((x)    => Console.WriteLine($"Execute CL"))
-                .WithParsed<ExecShell>((x) => Console.WriteLine($"Execute Shell"))
-                .WithParsed<ExecSql>((x)   => Console.WriteLine($"Execute SQL"))
+                .WithParsed<ExecCl>(x => Console.WriteLine($"Execute CL"))
+                .WithParsed<ExecShell>(x => Console.WriteLine($"Execute Shell"))
+                .WithParsed<ExecSql>(x => Console.WriteLine($"Execute SQL"))
                 .WithNotParsed(x => {
                     var helpText = HelpText.AutoBuild(parserResult, ht => {
                         ht.AutoHelp = false;
@@ -48,9 +48,9 @@ namespace Kanpachi.CLI{
             var parser = new Parser(with => with.HelpWriter = null);
             var parserResult = parser.ParseArguments<IfsGetDir, IfsGetFile, IfsLs>(baseCmd.SubArgs);
             parserResult
-                .WithParsed<IfsGetDir>((x)  => Console.WriteLine($"IFS get directory"))
-                .WithParsed<IfsGetFile>((x) => Console.WriteLine($"IFS get file"))
-                .WithParsed<IfsLs>((x)      => Console.WriteLine($"IFS list"))
+                .WithParsed<IfsGetDir>(x => Console.WriteLine($"IFS get directory"))
+                .WithParsed<IfsGetFile>(x => Console.WriteLine($"IFS get file"))
+                .WithParsed<IfsLs>(x => Console.WriteLine($"IFS list"))
                 .WithNotParsed(x => {
                     var helpText = HelpText.AutoBuild(parserResult, ht => {
                         ht.AutoHelp = false;
@@ -65,10 +65,10 @@ namespace Kanpachi.CLI{
             var parser = new Parser(with => with.HelpWriter = null);
             var parserResult = parser.ParseArguments<ProfileAdd, ProfileRm, ProfileLs, ProfileDft>(baseCmd.SubArgs);
             parserResult
-                .WithParsed<ProfileAdd>((x) => Console.WriteLine($"Add profile"))
-                .WithParsed<ProfileLs>((x)  => Console.WriteLine($"List profiles"))
-                .WithParsed<ProfileRm>((x)  => Console.WriteLine($"Remove profile"))
-                .WithParsed<ProfileDft>((x) => Console.WriteLine($"Set default profile"))
+                .WithParsed<ProfileAdd>(x => Console.WriteLine($"Add profile"))
+                .WithParsed<ProfileLs>(x => Console.WriteLine($"List profiles"))
+                .WithParsed<ProfileRm>(x => Console.WriteLine($"Remove profile"))
+                .WithParsed<ProfileDft>(x => Console.WriteLine($"Set default profile"))
                 .WithNotParsed(x => {
                     var helpText = HelpText.AutoBuild(parserResult, ht => {
                         ht.AutoHelp = false;
@@ -83,11 +83,11 @@ namespace Kanpachi.CLI{
             var parser = new Parser(with => with.HelpWriter = null);
             var parserResult = parser.ParseArguments<QsysGetLib, QsysGetMbr, QsysGetSpf, QsysLsLib, QsysLsSpf>(baseCmd.SubArgs);
             parserResult
-                .WithParsed<QsysGetLib>((x) => Console.WriteLine($"QSYS get lib"))
-                .WithParsed<QsysGetMbr>((x) => Console.WriteLine($"QSYS get member"))
-                .WithParsed<QsysGetSpf>((x) => Console.WriteLine($"QSYS get source physical file"))
-                .WithParsed<QsysLsLib>((x)  => Console.WriteLine($"QSYS list library"))
-                .WithParsed<QsysLsSpf>((x)  => Console.WriteLine($"QSYS list source physical file"))
+                .WithParsed<QsysGetLib>(x => Console.WriteLine($"QSYS get lib"))
+                .WithParsed<QsysGetMbr>(x => Console.WriteLine($"QSYS get member"))
+                .WithParsed<QsysGetSpf>(x => Console.WriteLine($"QSYS get source physical file"))
+                .WithParsed<QsysLsLib>(x => Console.WriteLine($"QSYS list library"))
+                .WithParsed<QsysLsSpf>(x => Console.WriteLine($"QSYS list source physical file"))
                 .WithNotParsed(x => {
                     var helpText = HelpText.AutoBuild(parserResult, ht => {
                         ht.AutoHelp = false;
@@ -107,13 +107,12 @@ namespace Kanpachi.CLI{
             // config   set   -k sshport -v 22
 
             // Parse base commands
-            
             Parser.Default.ParseArguments<ConfigCmd, ExecCmd, IfsCmd, ProfileCmd, QsysCmd>(args)
-                .WithParsed<ConfigCmd>(ParseConfigCmd)
-                .WithParsed<ExecCmd>(ParseExecCmd)
-                .WithParsed<IfsCmd>(ParseIfsCmd)
-                .WithParsed<ProfileCmd>(ParseProfileCmd)
-                .WithParsed<QsysCmd>(ParseQsysCmd)
+                .WithParsed<ConfigCmd>(x => ParseConfigCmd(x))
+                .WithParsed<ExecCmd>(x => ParseExecCmd(x))
+                .WithParsed<IfsCmd>(x => ParseIfsCmd(x))
+                .WithParsed<ProfileCmd>(x => ParseProfileCmd(x))
+                .WithParsed<QsysCmd>(x => ParseQsysCmd(x))
             ;
         }
     }
